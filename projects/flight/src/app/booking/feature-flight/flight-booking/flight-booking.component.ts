ef82f41239a5d6ac3d7e94f3a9a0cfdaed90863e
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-12 16:33:30 +0100

    feat: add navigation config w/ local provider and provider directive w/ property binding

--- projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
@@ -1,17 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from '../../../shared/ui-common';
+import { provideNavigationConfig, provideNavigationService } from '../../../shared/logic-navigation';
+import { NavigationProviderDirective } from '../../../shared/ui-common/navigation/navigation-provider.directive';
 
 
 @Component({
   selector: 'app-flight-booking',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavigationComponent,
+    NavigationProviderDirective
+  ],
+  providers: [
+    provideNavigationService([
+      {
+        route: 'flight/search',
+        label: 'Flight Search',
+        icon: 'flight'
+      }
+    ])
   ],
   template: `
     <div>
+      <app-navigation class="nav-standalone" />
+      <app-navigation [navConfig]="navState" class="nav-standalone" />
       <router-outlet></router-outlet>
     </div>
   `
 })
-export class FlightBookingComponent {}
+export class FlightBookingComponent {
+  navState = [{
+    route: 'flight/departures',
+    label: 'Departures',
+    icon: 'departures'
+  }];
+}
